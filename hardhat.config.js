0e685c8a7c66de3238463252ed430c2a4e5c06d3
hack3r-0m <oparikh113@gmail.com>
2021-07-02 09:15:13 +0530

    new: adding helpers, utils & interfaces

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,20 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.7.3',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+      },
+      {
+        version: '0.7.6',
+      },
+      {
+        version: '0.6.12',
+      },
+      {
+        version: '0.5.16',
+      },
+    ],
+  },
 };
